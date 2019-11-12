b8b6a5d80d7f870b219867c7dcb273318c9f9bd2
Sean Kim <templetonsean2006@gmail.com>
2019-11-11 16:30:32 -0800

    second

--- Contest 2018-2019/src/billboard.java
+++ Contest 2018-2019/src/billboard.java
@@ -24,39 +24,36 @@ public class billboard {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		boards = new boolean[1000][1000];
-		int x1 = in.nextInt();
-		int x2 = in.nextInt();
-		int y1 = in.nextInt();
-		int y2 = in.nextInt();
-		boards[x1][x2] = true;
-		boards[y1][y2] = true;
-		for(int i = x1; i < y1; i++) {
-			for(int j = x2; j < y2; j++) {
-				boards[i][j] = true;
-			}
-		}
-	}
-	public static void solve() {
-		int x1 = in.nextInt();
-		int x2 = in.nextInt();
-		int y1 = in.nextInt();
-		int y2 = in.nextInt();
-		boards[x1][x2] = true;
-		boards[y1][y2] = true;
-		for(int i = x1; i < y1; i++) {
-			for(int j = x2; j < y2; j++) {
-				boards[i][j] = false;
-			}
-		}
-		cnt = 0;
-		for(int i = 0; i < 1000; i++) {
-			for(int j = 0; j < 1000; j++) {
-				if(boards[i][j] == true) {
-					cnt ++;
+		boards = new boolean[2001][2001];
+      
+        cnt = 0;
+        for(int k = 0; k < 2; k++){
+          	int x1 = in.nextInt()+1000;
+			int y1 = in.nextInt()+1000;
+			int x2 = in.nextInt()+1000;
+			int y2 = in.nextInt()+1000;
+			
+			for(int i = x1; i < x2; i++) {
+				for(int j = y1; j < y2; j++) {
+					boards[i][j] = true;
+                  	cnt++;
 				}
 			}
+        }
+		
+	}
+	public static void solve() { 
+		int x1 = in.nextInt()+1000;
+		int y1 = in.nextInt()+1000;
+		int x2 = in.nextInt()+1000;
+		int y2 = in.nextInt()+1000;
+		
+		for(int i = x1; i < x2; i++) {
+			for(int j = y1; j < y2; j++) {
+				if(boards[i][j]) cnt--;
+			}
 		}
+		
 		out.println(cnt);
 	}
 }
